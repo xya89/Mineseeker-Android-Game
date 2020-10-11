69a0712a0bcf7ca5e686f254e8ea8f28a032d358
xya89 <xiaoyang_yang@sfu.ca>
2020-10-10 23:02:31 -0700

    Add showMineImage() function - if the button is a mine, onClick shows the landmine image.
    //TODO:
    1. need manager function to determine which buttons are mines.
    2. delete temporary testing functions inside GameBoard Activity.

---
+++ app/src/main/java/Model/Cell.java
@@ -0,0 +1,40 @@
+package Model;
+
+public class Cell {
+
+    private boolean isMine;
+    private boolean isRevealed;
+
+    // if the cell is not mine, then cell contains a number
+    private int mineCount;
+
+
+    // Add singleton support to Cell
+    private static Cell instance;
+    private Cell() {
+        // private to prevent anyone else from instantiating
+    }
+    public static Cell getInstance() {
+        if (instance == null) {
+            instance = new Cell();
+        }
+        return instance;
+    }
+
+    public int updateCount() {
+        return mineCount ++ ;
+    }
+
+    public boolean getIsMine() {
+        return isMine;
+    }
+    public void setMine(boolean mine) {
+        isMine = mine;
+    }
+    public boolean getIsRevealed() {
+        return isRevealed;
+    }
+    public void setRevealed(boolean revealed) {
+        isRevealed = revealed;
+    }
+}
--- app/src/main/java/cmpt276Asgn3/example/com/GameBoard.java
+++ app/src/main/java/cmpt276Asgn3/example/com/GameBoard.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,10 +15,29 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.Toast;
 
+import Model.Cell;
+
 public class GameBoard extends AppCompatActivity {
 
     private static final int NUM_ROWS = 4;
     private static final int NUM_COLS = 7;
+
+
+
+
+    private Cell cell = Cell.getInstance();
+
+    // TODO: use rand to assign mines: determine if the cell is mine;
+    // TODO: upon completion of the previous steps, delete the following codes;
+
+    Button buttons[][] = new Button[NUM_ROWS][NUM_COLS];
+
+
+    // Temporary test variables
+    private void setMineCells() {
+        cell.setMine(true);
+    }
+
 
 
     @Override
@@ -22,12 +45,12 @@ public class GameBoard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_board);
 
-        popoulateButtons();
+        populateButtons();
 
     }
 
-    private void popoulateButtons() {
-        TableLayout table = (TableLayout) findViewById(R.id.TBL_gamepad);
+    private void populateButtons() {
+        TableLayout table = findViewById(R.id.TBL_gamepad);
 
         for (int row = 0;  row < NUM_ROWS; row++) {
             TableRow tableRow = new TableRow(this);
@@ -39,30 +62,81 @@ public class GameBoard extends AppCompatActivity {
             table.addView(tableRow);
 
             for (int col = 0; col < NUM_COLS; col++){
+
+                final int FINAL_COL = col;
+                final int FINAL_ROW = row;
+
                 Button button = new Button(this);
                 button.setLayoutParams(new TableRow.LayoutParams(
                         TableRow.LayoutParams.MATCH_PARENT,
                         TableRow.LayoutParams.MATCH_PARENT,
                         1.0f));
 
+                // delete this
+                button.setText("" + col + "," + row);
+
+                button.setPadding(0,0,0,0);
                 button.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        gridButtonClicked();
+                        gridButtonClicked(FINAL_COL, FINAL_ROW);
+
                     }
                 });
 
                 tableRow.addView(button);
+                buttons[row][col] = button;
             }
         }
     }
 
-    private void gridButtonClicked() {
-        Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show();
+    private void gridButtonClicked(int col, int row) {
+        Toast.makeText(this, "Button clicked: " + col + "," + row, Toast.LENGTH_SHORT).show();
+
+        Button button = buttons[row][col];
+
+        // Lock Button Sizes;
+        lockButtonSizes();
+
+        // temp functions, delete this
+        setMineCells();
+
+        if (cell.getIsMine()){
+            // TODO: cell button becomes a image of mine
+            showMineImage(button);
+
+        }
+        else if (!cell.getIsMine()){
+            // TODO: cell button display number of mines left in the same row and same col.
+        }
+    }
+
+    private void showMineImage(Button button) {
+        int newWidth = button.getWidth();
+        int newHeight = button.getHeight();
+        Bitmap originalBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.rust_landmine_png);
+        Bitmap scaleBitmap = Bitmap.createScaledBitmap(originalBitmap, newWidth, newHeight, true);
+        Resources resource = getResources();
+        button.setBackground(new BitmapDrawable(resource, scaleBitmap));
+    }
+
+    private void lockButtonSizes() {
+        for (int row = 0; row < NUM_ROWS; row ++ ) {
+            for (int col = 0; col < NUM_COLS; col ++) {
+                Button button = buttons[row][col];
+
+                int width = button.getWidth();
+                button.setMinWidth(width);
+                button.setMaxWidth(width);
+
+                int height = button.getHeight();
+                button.setMinHeight(height);
+                button.setMaxHeight(height);
+            }
+        }
     }
 
     public static Intent makeIntent(Context context) {
-        Intent intent = new Intent(context, GameBoard.class);
-        return intent;
+        return new Intent(context, GameBoard.class);
     }
 }
